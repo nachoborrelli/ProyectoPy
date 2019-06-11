276110cfde63ab10865efc6581a31153d3c47c72
Matigeber <51551162+Matigeber@users.noreply.github.com>
2019-06-11 15:33:06 -0300

    Sopa comentada

--- SopaDeLetras/Sopa.py
+++ SopaDeLetras/Sopa.py
@@ -16,36 +16,39 @@ def bienvenida():
     event,  values = bienvenido.Read(timeout=4000)
 
 def draw_grid(window,palMax,g,coordenadas):
-    '''
+    ''' Dibuja con letras random (POR AHORA) la matriz. A su vez, guarda en un diccionario auxiliar
+    con las cordenadas como clave y su letra como valor.
         '''
     for row in range(len(palMax)):
         for col in range(len(palMax)):
             g.DrawRectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3),
-                            (col * BOX_SIZE + BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black')
+                            (col * BOX_SIZE + BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black')  #Creo la grilla
             letra = random.choice(string.ascii_uppercase)       #Me guardo una letra random
-            g.DrawText('{}'.format(letra), (col * BOX_SIZE + 15, row * BOX_SIZE + 15), font='Courier 25')
-            coordenadas[(col,row)]=letra
+            g.DrawText('{}'.format(letra), (col * BOX_SIZE + 15, row * BOX_SIZE + 15), font='Courier 25')  #Escribo la letra.
+            coordenadas[(col, row)] = letra         #Generacion del diccionario auxiliar.
     return coordenadas
 
-
-def Pintar(coordenadas,borrados,g,punto):
+def Pintar(coordenadas,pintados,g,punto):
+    '''  Se ocupa de indicar como marcada una casilla pintandola en gris.
+        '''
     g.DrawRectangle((punto[0] * BOX_SIZE + 5, punto[1] * BOX_SIZE + 3),
                     (punto[0] * BOX_SIZE + BOX_SIZE + 5, punto[1] * BOX_SIZE + BOX_SIZE + 3), line_color='black',
-                    fill_color='red')
+                    fill_color='grey72')
     g.DrawText('{}'.format(coordenadas[punto]), (punto[0] * BOX_SIZE + 15, punto[1]* BOX_SIZE + 15),
                font='Courier 25')
-    borrados[punto] = coordenadas[punto]
-    del coordenadas[punto]
+    pintados[punto] = coordenadas[punto]  #Mantengo una estructura con solo las casillas pintadas.
+    del coordenadas[punto]                  #Y las saco de mi estructura auxiliar.
 
 
-def Despintar(coordenadas,borrados,g,punto):
+def Despintar(coordenadas,pintados,g,punto):
     g.DrawRectangle((punto[0] * BOX_SIZE + 5, punto[1] * BOX_SIZE + 3),
                     (punto[0]* BOX_SIZE + BOX_SIZE + 5, punto[1] * BOX_SIZE + BOX_SIZE + 3), line_color='black',
                     fill_color='white')
     g.DrawText('{}'.format(borrados[punto]), (punto[0]* BOX_SIZE + 15, punto[1] * BOX_SIZE + 15),
                font='Courier 25')
-    coordenadas[punto] = borrados[punto]
-    del borrados[punto]
+    coordenadas[punto] = pintados[punto]   #Devuelvo la casilla de la estructura de pintados a mi auxiliar
+    del pintados[punto]
+
 #--------------------------------------- Layouts ---------------------------------------
 
 layout = [
@@ -63,17 +66,19 @@ window = sg.Window('Window Title', ).Layout(layout).Finalize()
 #bienvenida()
 #draw_grid(window)
 g = window.FindElement('_GRAPH_')
-BOX_SIZE = 25
-palabras=['milanesa','telefono','galletitas','computadora','sdkjfisdfjdsf']
+BOX_SIZE = 25      #Tamaño de las casillas
+palabras=['milanesa', 'telefono', 'galletitas', 'computadora', 'sdkjfisdfjdsf']   #Estas deben llegar por parametro
 max=-1
-for pal in palabras:
-    if (len(pal)>max):
-        palMax=pal
-        max=len(pal)
-print(palMax)
-coordenadas={}
-coordenadas=draw_grid(window,palMax,g,coordenadas)
-borrados={}
+for pal in palabras:     #Me quedo con la palabra mas larga.
+    if len(pal) > max:
+        palMax = pal
+        max = len(pal)
+print(palMax)    #Sacar.
+coordenadas = {}
+coordenadas = draw_grid(window, palMax, g, coordenadas)
+borrados = {}
+
+
 while True:             # Event Loop
     event, values = window.Read()
     if event is None or event == 'Exit':
@@ -81,12 +86,12 @@ while True:             # Event Loop
     mouse = values['_GRAPH_']
     if event == '_GRAPH_':
         if mouse == (None, None):
-            continue            #Pass vs continue?
-        x = mouse[0] // BOX_SIZE
-        y = mouse[1] // BOX_SIZE
-        #letter_location = (box_x * BOX_SIZE + 18, box_y * BOX_SIZE + 17)
-        punto=(x,y)
-        if(punto in coordenadas.keys()):
-           Pintar(coordenadas,borrados,g,punto)
+            pass            #Pass vs continue?
         else:
-            Despintar(coordenadas,borrados,g,punto)
+            x = mouse[0] // BOX_SIZE
+            y = mouse[1] // BOX_SIZE
+            punto = (x, y)
+            if punto in coordenadas.keys():
+                Pintar(coordenadas, borrados, g, punto)
+            else:
+                Despintar(coordenadas, borrados, g, punto)
