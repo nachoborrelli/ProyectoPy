fe955bcd11b8f650ac37023961b6c028747af535
Matigeber <51551162+Matigeber@users.noreply.github.com>
2019-06-11 14:49:18 -0300

    Sopa comentada

--- Mati/SopaPrueba.py
+++ Mati/SopaPrueba.py
@@ -12,22 +12,41 @@ def bienvenida():
 
                          ]
 
-
     bienvenido = sg.Window('Bienvenido!', layout=layout_bienvenido)
     event,  values = bienvenido.Read(timeout=4000)
 
 def draw_grid(window,palMax,g,coordenadas):
-
+    '''
+        '''
     for row in range(len(palMax)):
         for col in range(len(palMax)):
             g.DrawRectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3),
                             (col * BOX_SIZE + BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black')
-            # g.DrawText('{}'.format(row * 6 + col + 1), (col * BOX_SIZE + 10, row * BOX_SIZE + 8))
-            letra=random.choice(string.ascii_uppercase)
+            letra = random.choice(string.ascii_uppercase)
             g.DrawText('{}'.format(letra), (col * BOX_SIZE + 15, row * BOX_SIZE + 15),
                        font='Courier 25')
             coordenadas[(col,row)]=letra
     return coordenadas
+
+
+def Pintar(coordenadas,borrados,g):
+    g.DrawRectangle((box_x * BOX_SIZE + 5, box_y * BOX_SIZE + 3),
+                    (box_x * BOX_SIZE + BOX_SIZE + 5, box_y * BOX_SIZE + BOX_SIZE + 3), line_color='black',
+                    fill_color='red')
+    g.DrawText('{}'.format(coordenadas[punto]), (box_x * BOX_SIZE + 15, box_y * BOX_SIZE + 15),
+               font='Courier 25')
+    borrados[punto] = coordenadas[punto]
+    del coordenadas[punto]
+
+
+def Despintar(coordenadas,borrados,g):
+    g.DrawRectangle((box_x * BOX_SIZE + 5, box_y * BOX_SIZE + 3),
+                    (box_x * BOX_SIZE + BOX_SIZE + 5, box_y * BOX_SIZE + BOX_SIZE + 3), line_color='black',
+                    fill_color='white')
+    g.DrawText('{}'.format(borrados[punto]), (box_x * BOX_SIZE + 15, box_y * BOX_SIZE + 15),
+               font='Courier 25')
+    coordenadas[punto] = borrados[punto]
+    del borrados[punto]
 #--------------------------------------- Layouts ---------------------------------------
 
 layout = [
@@ -58,30 +77,17 @@ coordenadas=draw_grid(window,palMax,g,coordenadas)
 borrados={}
 while True:             # Event Loop
     event, values = window.Read()
-    print(event, values)
     if event is None or event == 'Exit':
         break
     mouse = values['_GRAPH_']
     if event == '_GRAPH_':
         if mouse == (None, None):
             continue            #Pass vs continue?
-        box_x = mouse[0] // BOX_SIZE
-        box_y = mouse[1] // BOX_SIZE
-        letter_location = (box_x * BOX_SIZE + 18, box_y * BOX_SIZE + 17)
-        print(box_x, box_y)
-        punto=(box_x,box_y)
+        x = mouse[0] // BOX_SIZE
+        y = mouse[1] // BOX_SIZE
+        #letter_location = (box_x * BOX_SIZE + 18, box_y * BOX_SIZE + 17)
+        punto=(x,y)
         if(punto in coordenadas.keys()):
-            g.DrawRectangle((box_x * BOX_SIZE + 5, box_y * BOX_SIZE + 3),
-                            (box_x * BOX_SIZE + BOX_SIZE + 5, box_y * BOX_SIZE + BOX_SIZE + 3), line_color='black',
-                            fill_color='red')
-            g.DrawText('{}'.format(coordenadas[punto]), (box_x * BOX_SIZE + 15, box_y * BOX_SIZE + 15),
-                       font='Courier 25')
-            borrados[punto]=coordenadas[punto]
-            del coordenadas[punto]
+           Pintar(coordenadas,borrados,g)
         else:
-            g.DrawRectangle((box_x * BOX_SIZE + 5, box_y * BOX_SIZE + 3),
-                            (box_x * BOX_SIZE + BOX_SIZE + 5, box_y * BOX_SIZE + BOX_SIZE + 3), line_color='black',fill_color='white')
-            g.DrawText('{}'.format(borrados[punto]), (box_x * BOX_SIZE + 15, box_y * BOX_SIZE + 15),
-                       font='Courier 25')
-            coordenadas[punto]=borrados[punto]
-            del borrados[punto]
+            Despintar(coordenadas,borrados,g)
