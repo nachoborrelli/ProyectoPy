f0f6b606eccb55e94cb4c56c54a16c934972209b
Matigeber <51551162+Matigeber@users.noreply.github.com>
2019-07-11 10:33:17 -0300

    Prueba mati arreglado

--- Raspberry/Prueba mati.py
+++ Raspberry/Prueba mati.py
@@ -1,7 +1,8 @@
 #import Adafruit_DHT
-import time
 from datetime import datetime
+import time
 import json
+import os
 
 # #=================================SENSOR======================================================
 # class Temperatura:
@@ -13,32 +14,35 @@ import json
 #         humedad, temperatura = Adafruit_DHT.read_retry(self._sensor, self._data_pin)
 #         return {'temperatura': temperatura, 'humedad': humedad}
 
-def AgregarDatos(datos,dic):
+def AgregarDatos(datos):
+    dic = {}
     dic['Temperatura'] = datos[0]
     dic['Humedad'] = datos[1]
-    dic['Fecha'] = '20190707'
+    dic['Fecha'] = str(datetime.fromtimestamp(time.time()))
+    return dic
 
 def datosSensor():
     return(20,60)
 #temp = Temperatura()
-dic = {}
 oficinas = {}
 oficinas['Oficina 1'] = []
 oficinas ['Oficina 2'] = []
 oficinas ['Oficina 3'] = []
-try:
-    jsonfile = open('datos-oficinas.json', 'x')
-except FileExistsError:
-    jsonfile = open('datos-oficinas.json', 'a')
+
 while True:
+    try:
+        jsonfile = open('datos-oficinas.json', 'x')
+    except FileExistsError:
+        jsonfile = open('datos-oficinas.json', 'r+')
     time.sleep(2)
     for i in range(1,4):
         datos = datosSensor()
         print(type(datos))
         print(datos)
-        AgregarDatos(datos,dic)
-        oficinas['Oficina {}'.format(i)].append(dic)
-        json.dump(oficinas['Oficina {}'.format(i)],jsonfile)
-        jsonfile.write('\n')
-        dic.clear()
-    break
+        oficinas['Oficina {}'.format(i)].append(AgregarDatos(datos))
+    json.dump(oficinas, jsonfile)
+    jsonfile.close()
+    #jsonfile = open('datos-oficinas.json', 'r+')
+    #oficinas = json.load(jsonfile)
+    #jsonfile.write('\n')
+
